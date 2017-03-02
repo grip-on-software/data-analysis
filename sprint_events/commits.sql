a903a32fee924f744576de7184d11aae7e36b756
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-03-02 14:28:10 +0100

    Timeline events: Match sprint commits differently
    Instead of searching the commits table for commit dates between the
    sprint start and end, use the sprint ID that was already matched during
    data gathering. This has the only difference that overlapping sprints do
    not show the commit in all involved sprints: the sprint with the closest
    start date that contains the commit date wins. Additionally, this may
    later be useful if commits are individually matched to issue or sprint
    using, e.g., commit message identifiers.

--- sprint_events/commits.sql
+++ sprint_events/commits.sql
@@ -1,4 +1,4 @@
-SELECT project."name" AS project_name, sprint.sprint_id AS sprint_id, sprint."name" AS sprint_name, commits.commit_date AS date, 'commits' AS type
+SELECT project."name" AS project_name, sprint.sprint_id AS sprint_id, sprint."name" AS sprint_name, commits.commit_date AS date
 FROM gros.commits
-JOIN gros.sprint ON commits.commit_date BETWEEN sprint.start_date AND sprint.end_date
+JOIN gros.sprint ON commits.sprint_id = sprint.sprint_id
 JOIN gros.project ON commits.project_id = project.project_id
