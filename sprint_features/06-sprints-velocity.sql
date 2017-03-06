2217ea265d7beaef83e2e495906e0d43e8f97102
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-03-06 14:43:20 +0100

    Sprint features: Use schema before table name

--- sprint_features/06-sprints-velocity.sql
+++ sprint_features/06-sprints-velocity.sql
@@ -1,5 +1,5 @@
 SELECT issue.project_id, issue.sprint_id, CAST(SUM(story_points) AS float)/(days/7*5) AS velocity
-FROM gros.issue LEFT OUTER JOIN sprint ON issue.project_id = sprint.project_id AND issue.sprint_id = sprint.sprint_id,
+FROM gros.issue LEFT OUTER JOIN gros.sprint ON issue.project_id = sprint.project_id AND issue.sprint_id = sprint.sprint_id,
 (SELECT issue_id, MAX(changelog_id) AS max_changelog_id FROM gros.issue GROUP BY issue_id) AS maxdata,
 (SELECT sprint_id, start_date, EXTRACT(day FROM (end_date - start_date)) AS days FROM gros.sprint) AS weekdata
 -- Additional joins
