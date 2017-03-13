3175c724f8589bedcfe70aacd70ad7fc4f577393
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-03-13 15:37:53 +0100

    Sprint events: Fix story query
    Broken after rename.

--- sprint_events/story.sql
+++ sprint_events/story.sql
@@ -1,5 +1,5 @@
 SELECT project."name" AS project_name, sprint.sprint_id AS sprint_id, sprint."name" AS sprint_name, issuedata.date AS date, issuedata.end_date AS end_date 
-FROM (SELECT start_issue.project_id, start_issue.sprint_id, start_issue.issue_id, MIN(start_issue.updated) AS date, MIN(end_issue.updated) AS end_date
+FROM (SELECT start_issue.project_id, start_issue.sprint_id, start_issue.issue_id, MIN(start_issue.updated) AS date, MIN(issue.updated) AS end_date
 	FROM gros.issue AS start_issue
 	JOIN gros.issue AS issue ON start_issue.issue_id = issue.issue_id AND start_issue.changelog_id < issue.changelog_id
 	WHERE start_issue.type = 7 AND start_issue.status = 3 AND ${issue_done}
