0a85c2f4fe74d09295c4761d42670e68c6bd9c99
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-03-13 17:08:38 +0100

    Sprint features: Fix query

--- sprint_features/06-sprints-velocity.sql
+++ sprint_features/06-sprints-velocity.sql
@@ -1,7 +1,7 @@
 SELECT issue.project_id, issue.sprint_id, ${velocity} AS velocity
 FROM gros.issue LEFT OUTER JOIN gros.sprint ON issue.project_id = sprint.project_id AND issue.sprint_id = sprint.sprint_id,
 (SELECT issue_id, MAX(changelog_id) AS max_changelog_id FROM gros.issue GROUP BY issue_id) AS maxdata,
-(SELECT sprint_id, start_date, ${sprint_days} AS days FROM gros.sprint) AS weekdata
+(SELECT sprint_id, start_date, ${sprint_days} AS sprint_days FROM gros.sprint) AS weekdata
 -- Additional joins
 WHERE issue.issue_id = maxdata.issue_id AND issue.changelog_id = max_changelog_id
 AND issue.sprint_id = weekdata.sprint_id
