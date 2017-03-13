a4393b0a1a42525b3646f67e131b94a4f2003460
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-03-13 16:39:32 +0100

    Sprint feautres: Fix query

--- sprint_features/04-not-done-status.sql
+++ sprint_features/04-not-done-status.sql
@@ -1,8 +1,8 @@
 SELECT issue.project_id, issue.sprint_id, COUNT(*) AS num_not_done FROM gros.issue,
 (SELECT DISTINCT(issue.issue_id) FROM gros.issue, gros.sprint,
 	(SELECT issue_id, MAX(changelog_id) AS changelog_id FROM gros.issue GROUP BY issue_id) AS max_issue
-	WHERE issue.issue_id = maxdata.issue_id
-	AND issue.changelog_id = maxdata.max_changelog_id
+	WHERE issue.issue_id = max_issue.issue_id
+	AND issue.changelog_id = max_issue.changelog_id
 	AND issue.sprint_id = sprint.sprint_id
 	AND ${issue_not_done} AND ${issue_overdue}) AS issuedata
 WHERE issue.issue_id = issuedata.issue_id
