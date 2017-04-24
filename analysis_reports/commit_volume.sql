9f4f4bf701c7c435dc63c3a6ec5768d305eed9d8
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-04-24 11:40:36 +0200

    Analysis reports: Exclude automated developers
    Exclude build server accounts when calculating commit volume and sprint
    developer count. This is indicated by a special value in the VCS
    developer table.

--- analysis_reports/commit_volume.sql
+++ analysis_reports/commit_volume.sql
@@ -1,3 +1,5 @@
 SELECT project_id, CAST(commit_date AS DATE) AS commit_day, COUNT(*) AS value
 FROM gros.commits
+JOIN gros.vcs_developer ON commits.developer_id = vcs_developer.alias_id
+WHERE vcs_developer.jira_dev_id <> -1
 GROUP BY project_id, commit_day
--- analysis_reports/developers.sql
+++ analysis_reports/developers.sql
@@ -3,6 +3,8 @@
 SELECT sprint_devs.project_id, sprint_devs.commit_date, COUNT(*) AS value FROM (
     SELECT DISTINCT commits.project_id, CAST((CASE WHEN sprint.start_date IS NULL THEN commits.commit_date ELSE sprint.start_date END) AS DATE) AS commit_date, commits.developer_id FROM gros.commits
 	LEFT JOIN gros.sprint ON commits.sprint_id = sprint.sprint_id
+	JOIN gros.vcs_developer ON commits.developer_id = vcs_developer.alias_id
+	WHERE vcs_developer.jira_dev_id <> -1
 ) AS sprint_devs
 GROUP BY sprint_devs.project_id, sprint_devs.commit_date
 ORDER BY sprint_devs.project_id, sprint_devs.commit_date
