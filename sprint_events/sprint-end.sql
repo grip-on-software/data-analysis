14a23a22311679cae3dfb912b538d2fa19bd72d6
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-05-11 10:40:16 +0200

    Timeline: Use sprint complete date if available
    This fixes problems with incorrectly projected dates by using the
    'complete date' of the sprint, which is also shown in the JIRA interface
    when it is set.

--- sprint_events/sprint-end.sql
+++ sprint_events/sprint-end.sql
@@ -1,3 +1,3 @@
-SELECT project."name" AS project_name, sprint.sprint_id AS sprint_id, sprint."name" AS sprint_name, sprint.end_date AS date
+SELECT project."name" AS project_name, sprint.sprint_id AS sprint_id, sprint."name" AS sprint_name, COALESCE(sprint.complete_date, sprint.end_date) AS date
 FROM gros.sprint
 JOIN gros.project ON sprint.project_id = project.project_id
