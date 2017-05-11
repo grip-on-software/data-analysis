c709d29a66103690fe811a063384df0477241ba1
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-05-11 11:07:46 +0200

    Timeline: Correct variable name

--- sprint_events/sprint-end.sql
+++ sprint_events/sprint-end.sql
@@ -1,3 +1,3 @@
-SELECT project."name" AS project_name, sprint.sprint_id AS sprint_id, sprint."name" AS sprint_name, ${sprint_end} AS date
+SELECT project."name" AS project_name, sprint.sprint_id AS sprint_id, sprint."name" AS sprint_name, ${sprint_close} AS date
 FROM gros.sprint
 JOIN gros.project ON sprint.project_id = project.project_id
