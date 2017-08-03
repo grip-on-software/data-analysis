1662b00e433ff740a1c0f6fb32818c74d86f2a6a
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-08-03 14:10:21 +0200

    Sprint features: Add exclude argument
    This argument already existed in timeline.r, but not in features.r even
    though it is of use here as well.

--- features.r
+++ features.r
@@ -2,11 +2,13 @@
 # exports them to an ARFF file readable by Weka and other data mining tools.
 
 library(foreign) # For write.arff
+source('include/args.r')
 source('include/database.r')
 source('include/sprint_features.r')
 conn <- connect()
 
-result <- get_sprint_features(conn)
+exclude <- get_arg('--exclude')
+result <- get_sprint_features(conn, exclude)
 sprint_data <- result$data
 
 write.arff(sprint_data[,result$colnames], file='output/sprint_features.arff',
