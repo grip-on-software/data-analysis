57ee24e3bb3de83241eda1188f199bdfba0417df
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-08-14 17:04:59 +0200

    Features: Clean up MonetDB syntax
    Some versions of MonetDB do not allow parethesizing multiple tables into
    one JOIN, so just pretend we are (MonetDB figures out the best strategy
    to combine the tables anyway).

--- sprint_features/01-new-committers.sql
+++ sprint_features/01-new-committers.sql
@@ -6,18 +6,18 @@ SELECT new_developer.project_id, new_developer.sprint_id, COUNT(*) AS num_new_de
             THEN vcs_developer.jira_dev_id
             ELSE -commits1.developer_id
         END AS developer_id
-    FROM (
+    FROM
         (SELECT DISTINCT
             commits.project_id, commits.sprint_id, commits.developer_id
         FROM gros.commits) AS commits1
         LEFT JOIN gros.vcs_developer ON commits1.developer_id = vcs_developer.alias_id
-    )
+
     LEFT JOIN (
         (SELECT DISTINCT
             commits.project_id, commits.sprint_id, commits.developer_id
-        FROM gros.commits) AS commits2
+        FROM gros.commits)) AS commits2
         LEFT JOIN gros.vcs_developer AS developer2 ON commits2.developer_id = developer2.alias_id
-    )
+
     ON commits2.project_id = commits1.project_id
     AND (commits1.developer_id = -commits2.developer_id OR vcs_developer.alias_id = developer2.alias_id)
     AND commits2.sprint_id < commits1.sprint_id
