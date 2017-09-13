8e447480a798d4dc46fec5635eed8b66d05c7e8d
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-09-13 13:32:48 +0200

    Prediction: Add sprint dates to results
    This allows API users to build information about the sprint.

--- sprint_results.r
+++ sprint_results.r
@@ -15,14 +15,21 @@ projects <- dbGetQuery(conn, 'SELECT project.project_id, project."name" FROM gro
 
 write(toJSON(projects$name, auto_unbox=T),
 	  file=paste("output", "projects.json", sep="/"))
+patterns <- load_definitions('sprint_definitions.yml')
 for (idx in 1:length(results$projects)) {
 	project <- results$projects[idx]
 	project_name <- projects[project,'name'] 
+	query <- paste('SELECT sprint.start_date, ${sprint_close} AS close_date FROM gros.sprint WHERE sprint.project_id = ', projects[project,'project_id'], ' ORDER BY sprint.start_date', sep='')
+	item <- load_query(list(query=query), patterns)
+	sprint <- dbGetQuery(conn, item$query)
 	path <- paste("output", project_name, sep="/")
 	if (!dir.exists(path)) {
 		dir.create(path)
 	}
-	project_data <- list(project=project_name, sprint=results$sprints[idx],
+	sprint_id = results$sprints[idx]
+	project_data <- list(project=project_name, sprint=sprint_id,
+						 start_date=as.POSIXct(sprint$start_date[sprint_id]),
+						 end_date=as.POSIXct(sprint$close_date[sprint_id]),
 						 prediction=results$labels[idx],
 						 probability=results$probabilities[idx],
 						 risk=results$risks[idx],
