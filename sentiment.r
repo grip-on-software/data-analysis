0d7388ba64f3bd719f2c81d9beb8b9618bf087ab
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2017-12-06 17:09:38 +0100

    Sentiment: Fix problems
    - Sprint matching was incorrect, leading to way too many sprints.
    - Aggregate data correctly, and fix smoothening.
    - Write sentiment polarity feature to sprint features correctly.

--- sentiment.r
+++ sentiment.r
@@ -1,7 +1,10 @@
 source('include/args.r')
 source('include/database.r')
+source('include/log.r')
 library(pattern.nlp)
+library(foreign)
 library(ggplot2)
+library(jsonlite)
 library(zoo)
 
 output_directory <- get_arg('--output', default='output')
@@ -9,7 +12,7 @@ output_directory <- get_arg('--output', default='output')
 collect <- function() {
 	conn <- connect()
 	patterns <- load_definitions('sprint_definitions.yml')
-	query <- "SELECT project.name, sprint.sprint_id, sprint.start_date, comment.message FROM gros.comment JOIN gros.issue ON issue.issue_id = comment.issue_id AND issue.changelog_id = 0 JOIN gros.project ON issue.project_id = project.project_id LEFT JOIN gros.sprint ON issue.project_id = project.project_id AND comment.date BETWEEN sprint.start_date AND ${sprint_close} WHERE project.is_support_team = false AND message NOT LIKE 'Versie%\nGeslaagd:%' AND message <> '*Resolving* as a result of the *Resolve* action being applied to the parent.' ORDER BY project.name, sprint.sprint_id"
+	query <- "SELECT project.project_id, project.name, sprint.sprint_id, sprint.start_date, comment.message FROM gros.comment JOIN gros.issue ON issue.issue_id = comment.issue_id AND issue.changelog_id = 0 JOIN gros.project ON issue.project_id = project.project_id JOIN gros.sprint ON sprint.project_id = project.project_id AND comment.date BETWEEN sprint.start_date AND ${sprint_close} WHERE project.is_support_team = false AND message NOT LIKE 'Versie%\nGeslaagd:%' AND message <> '*Resolving* as a result of the *Resolve* action being applied to the parent.' ORDER BY project.name, sprint.sprint_id"
 	item <- load_query(list(query=query), patterns)
 	res <- dbGetQuery(conn, item$query)
 
@@ -37,8 +40,9 @@ load <- function(path) {
 }
 
 path <- paste(output_directory, 'sentiment.csv', sep='/')
+plot <- get_arg('--plot', default=F)
 if (get_arg('--load', default=F)) {
-	print('loading')
+	loginfo('loading from %s', path)
 	res <- load(path)
 } else {
 	res <- collect()
@@ -54,25 +58,45 @@ stats <- function(x) {
 }
 
 print(aggregate(as.numeric(as.character(res$polarity)), list(res$name), stats))
-boxplot(as.numeric(as.character(res$polarity)) ~ res$name, data=res)
+if (plot) {
+	boxplot(as.numeric(as.character(res$polarity)) ~ res$project_id, data=res)
+}
 
-invisible(lapply(split(res, res$name), function(project) {
-	values <- aggregate(list(polarity=as.numeric(as.character(project$polarity))), list(start_date=project$start_date), mean)
-	#indexes <- which(values$polarity != 0, arr.ind=T)
-	#print(indexes)
-	#data <- values[indexes,]
-	values$polarity <- rollmean(values$polarity, 10, fill=0)
-	print(values)
-	aspect_ratio = 1/1.6
-	date <- as.Date(values$start_date, '%Y-%m-%d')
-	ggplot(values, aes(x=date, y=values$polarity)) +
-		geom_line() +
-		geom_hline(yintercept=0, colour='grey') +
-		coord_equal(ratio=aspect_ratio) +
-		labs(title=paste("Sentiment for project", project[[1, 'name']]),
-			 x="Sprint date", y="Mean sentiment polarity") +
-		theme(plot.title=element_text(hjust=0.5), aspect.ratio=aspect_ratio)
-	ggsave(paste(output_directory,
-				 paste('sentiment', project[[1,'name']], 'png', sep='.'),
-				 sep='/'))
-}))
+projects <- lapply(split(res, res$name), function(project) {
+	values <- aggregate(list(polarity=as.numeric(as.character(project$polarity))),
+						list(start_date=as.Date(project$start_date, '%Y-%m-%d'), sprint_id=project$sprint_id),
+						mean)
+	values$project_id <- project[[1, 'project_id']]
+	values$sprint_num <- rownames(values)
+	if (dim(values)[1] >= 3) {
+		values$polarity <- rollmean(values$polarity, 3, fill=0)
+	}
+	if (plot) {
+		aspect_ratio <- 1/1.6
+		date <- values$start_date
+		print(date)
+		print(values$sprint_num)
+		ggplot(values, aes(x=date, y=values$polarity)) +
+			geom_line() +
+			geom_hline(yintercept=0, colour='grey') +
+			coord_equal(ratio=aspect_ratio) +
+			labs(title=paste("Sentiment for project", project[[1, 'name']]),
+			 	 x="Sprint date", y="Mean sentiment polarity") +
+			theme(plot.title=element_text(hjust=0.5), aspect.ratio=aspect_ratio)
+		ggsave(paste(output_directory,
+				 	 paste('sentiment', project[[1,'name']], 'png', sep='.'),
+				 	 sep='/'))
+	}
+	return(values)
+})
+
+sentiment_data <- do.call(rbind, projects)[,c('project_id', 'sprint_num', 'polarity')]
+#print(sentiment_data)
+features <- paste(output_directory, 'sprint_features.arff', sep='/')
+sprint_data <- read.arff(features)
+sprint_data <- merge(sprint_data, sentiment_data,
+					 by=c('project_id', 'sprint_num'), all.x=T)
+write.arff(sprint_data,
+		   file=paste(output_directory, 'sprint_features-sentiments.arff',
+		   			  sep='/'),
+		   relation="sprint_data")
