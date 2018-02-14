06c6e0a8d23e67efb71abf58e729b5dfc168f18e
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-02-14 17:08:53 +0100

    Sprint results: Handle new outputs
    - Analogy-based estimation provides analogous sprint indexes and labels,
      extract sprint information (mostly metadata) to add to the JSON output
    - Use a cache to keep project sprints for analogies
    - Sprint number (as in dataset) and sprint ID (as on JIRA) are now both
      in the result data, so that one can link to the sprint view
    - Ensure empty values are encoded as null rather than empty objects

--- sprint_results.r
+++ sprint_results.r
@@ -1,13 +1,32 @@
 # R script that combines output from a prediction model with other sprint data
 # such that it can be used by an API producer.
 
+library(foreign)
 library(jsonlite)
 source('include/args.r')
 source('include/database.r')
 source('include/log.r')
 source('include/project.r')
 
+sprint_cache <- vector("list")
+get_sprint <- function(project_id, sprint_id, cache=T) {
+	if (length(sprint_cache) >= project_id && mode(sprint_cache[[project_id]]) == "list") {
+		loginfo('Using cached sprints for project %d', project_id)
+		return(sprint_cache[[project_id]][sprint_id,])
+	}
+	query <- 'SELECT project.quality_display_name, sprint.sprint_id, sprint.name, sprint.start_date, ${sprint_close} AS close_date FROM gros.sprint JOIN gros.project ON sprint.project_id = project.project_id WHERE sprint.project_id = ${project_id} ORDER BY sprint.start_date'
+	item <- load_query(list(query=query), c(patterns, project_id=project_id))
+	time <- system.time(sprint <- dbGetQuery(conn, item$query))
+	loginfo('Obtained sprints of project %d in %f seconds',
+			project_id, time['elapsed'])
+	if (cache) {
+		sprint_cache[[project_id]] <<- sprint
+	}
+	return(sprint[sprint_id,])
+}
+
 input_file <- get_arg('--file', default='sprint_labels.json')
+feature_file <- get_arg('--features', default='output/sprint_features.arff')
 output_directory <- get_arg('--output', default='output')
 project_ids <- get_arg('--project-ids', default='0')
 if (project_ids != '0') {
@@ -15,6 +34,7 @@ if (project_ids != '0') {
 }
 
 results <- read_json(input_file, simplifyVector=T)
+features <- read.arff(feature_file)
 
 conn <- connect()
 
@@ -30,23 +50,45 @@ for (idx in 1:length(results$projects)) {
 		project_name <- paste("Proj", project_id, sep="")
 	}
 	projects <- c(projects, project_name)
-	query <- paste('SELECT sprint.start_date, ${sprint_close} AS close_date FROM gros.sprint WHERE sprint.project_id = ', project_id, ' ORDER BY sprint.start_date', sep='')
-	item <- load_query(list(query=query), patterns)
-	sprint <- dbGetQuery(conn, item$query)
+	sprint_id = results$sprints[idx]
+	sprint <- get_sprint(project_id, sprint_id,
+						 cache=!is.null(results$analogy_indexes))
+
+	if (!is.null(results$analogy_indexes)) {
+		analogies <- mapply(function(analogy, label) {
+			analogy_sprint <- get_sprint(features[analogy,"project_id"],
+										 features[analogy,"sprint_num"])
+			return(list(project=analogy_sprint$quality_display_name,
+						sprint=features[analogy,"sprint_num"],
+						id=analogy_sprint$sprint_id,
+						name=analogy_sprint$name,
+						start_date=as.POSIXct(analogy_sprint$start_date),
+						end_date=as.POSIXct(analogy_sprint$close_date),
+						label=label))
+		}, results$analogy_indexes[idx,], results$analogy_labels[idx,],
+		SIMPLIFY=F)
+	}
+	else {
+		analogies <- NULL
+	}
+
 	path <- paste(output_directory, project_name, sep="/")
 	if (!dir.exists(path)) {
 		dir.create(path)
 	}
-	sprint_id = results$sprints[idx]
-	project_data <- list(project=project_name, sprint=sprint_id,
-						 start_date=as.POSIXct(sprint$start_date[sprint_id]),
-						 end_date=as.POSIXct(sprint$close_date[sprint_id]),
+	project_data <- list(project=sprint$quality_display_name,
+						 sprint=sprint_id,
+						 id=sprint$sprint_id,
+						 name=sprint$name,
+						 start_date=as.POSIXct(sprint$start_date),
+						 end_date=as.POSIXct(sprint$close_date),
 						 prediction=results$labels[idx],
 						 probability=results$probabilities[idx],
 						 risk=results$risks[idx],
 						 metrics=results$metrics,
+						 analogies=analogies,
 						 configuration=results$configuration)
-	write(toJSON(project_data, auto_unbox=T),
+	write(toJSON(project_data, auto_unbox=T, null="null"),
 		  file=paste(path, "latest.json", sep="/"))
 }
 write(toJSON(projects, auto_unbox=T),
