2ebee32a8a403c6994ae7783a41186b1852b17cc
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-02-26 12:26:10 +0100

    Sprint features: Fix typo

--- sprint_features/00-sprint-gathered.sql
+++ sprint_features/00-sprint-gathered.sql
@@ -1,4 +1,4 @@
-SELECT sprint.project_id, sprint.sprint_id, CAST(MIN(update_tracker.update_date) > ${sprint_close} AS INT) AS sprint_id_complete
+SELECT sprint.project_id, sprint.sprint_id, CAST(MIN(update_tracker.update_date) > ${sprint_close} AS INT) AS sprint_is_complete
 FROM gros.update_tracker
 JOIN gros.sprint ON update_tracker.project_id = sprint.project_id
 GROUP BY sprint.project_id, sprint.sprint_id, sprint.end_date, sprint.complete_date
