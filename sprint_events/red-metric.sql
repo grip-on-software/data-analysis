54c196fe50afe2c2d709010bd6a30d4961b905ba
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-03-22 12:48:14 +0100

    Sprint event: Tighten conditions on red_metric
    Only consider red metrics that occurred during the current sprint and
    has started being red within the same sprint. This also speeds up the
    query about 6x.

--- sprint_events/red-metric.sql
+++ sprint_events/red-metric.sql
@@ -1,5 +1,6 @@
 SELECT DISTINCT ${s(project_name)} AS project_name, sprint.sprint_id AS sprint_id, sprint."name" AS sprint_name, since_date AS date
 FROM gros.metric_value
 JOIN gros.project ON metric_value.project_id = project.project_id
-LEFT OUTER JOIN gros.sprint ON metric_value.project_id = sprint.project_id AND metric_value.since_date BETWEEN sprint.start_date AND ${sprint_close}
-WHERE category = 'red' AND EXTRACT(day FROM date - since_date) > 7
+LEFT OUTER JOIN gros.sprint ON metric_value.project_id = sprint.project_id AND metric_value.sprint_id = sprint.sprint_id
+WHERE metric_value.since_date BETWEEN sprint.start_date AND ${sprint_close}
+AND category = 'red' AND EXTRACT(day FROM date - since_date) > 7
