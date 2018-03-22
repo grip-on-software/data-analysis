cb92fcf20e9a3046bea56f7698e2e10a532d4154
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-03-22 17:09:36 +0100

    Sprint features: Optimize team spirit metric
    Retrieve the latest metric value in a sprint in a way that does not use
    any joins but instead employs window functions.

--- sprint_features/02-team-spirit-metric.sql
+++ sprint_features/02-team-spirit-metric.sql
@@ -1,16 +1,18 @@
 -- Team spirit metric
-SELECT team_spirit.project_id, team_spirit.sprint_id, AVG(metric_value."value") AS team_spirit
-FROM gros.metric_value, (
-    SELECT metric_value.project_id, sprint.sprint_id, metric_value.metric_id, MAX(metric_value."date") AS max_date
-    FROM gros.metric_value
-    JOIN gros.metric
-    ON metric_value.metric_id = metric.metric_id
-    JOIN gros.sprint
-    ON metric_value.project_id = sprint.project_id
-    AND metric_value.sprint_id = sprint.sprint_id
-    WHERE metric_value."value" <> -1
-    AND metric.base_name = 'TeamSpirit'
-    GROUP BY metric_value.project_id, sprint.sprint_id, metric_value.metric_id
-) AS team_spirit
-WHERE metric_value."date" = team_spirit.max_date AND metric_value.metric_id = team_spirit.metric_id
-GROUP BY team_spirit.project_id, team_spirit.sprint_id
+SELECT project_id, sprint_id, value AS team_spirit FROM (
+	SELECT project_id, sprint_id, value, end_date,
+		ROW_NUMBER() OVER (
+			PARTITION BY project_id, sprint_id
+			ORDER BY project_id, sprint_id, end_date DESC
+		) AS rev_row
+	FROM (
+		SELECT metric_value.project_id, metric_value.sprint_id,
+			metric_value.value, MAX(metric_value.date) AS end_date
+		FROM gros.metric_value
+		JOIN gros.metric ON metric_value.metric_id = metric.metric_id
+		WHERE metric_value.value <> -1 AND metric.base_name = 'TeamSpirit'
+		GROUP BY metric_value.project_id, metric_value.sprint_id,
+			metric_value.value
+	) AS team_sprit_rows)
+AS team_sprit_metric
+WHERE rev_row = 1
