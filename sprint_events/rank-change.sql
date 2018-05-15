186f2d766ac1d146cb97ffdffc35713a56f0d262
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-05-15 11:57:49 +0200

    Sprint events: Exclude consecutive rank changes

--- sprint_events/rank-change.sql
+++ sprint_events/rank-change.sql
@@ -2,5 +2,8 @@ SELECT ${s(project_name)} AS project_name, sprint.sprint_id AS sprint_id, sprint
 FROM gros.issue
 LEFT OUTER JOIN gros.sprint ON issue.project_id = sprint.project_id AND issue.sprint_id = sprint.sprint_id
 JOIN gros.project ON issue.project_id = project.project_id
-WHERE issue.rank_change IS NOT NULL AND updated < ${sprint_close}
+JOIN gros.issue AS older_issue ON issue.issue_id = older_issue.issue_id AND issue.changelog_id = older_issue.changelog_id + 1 
+WHERE issue.rank_change IS NOT NULL
+AND (older_issue.rank_change IS NULL OR older_issue.rank_change <> issue.rank_change)
+AND issue.updated < ${sprint_close}
 ORDER BY project.project_id, sprint.start_date, issue.updated
