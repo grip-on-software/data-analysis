551cfce0ffc5a8e717bcdfe1271e17f91676ff5d
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-09-26 13:23:55 +0200

    Backlog size interval: Improvements
    - Add options
    - Exclude done issues
    - Correct output
    - Improve logging

--- backlog-size-interval.r
+++ backlog-size-interval.r
@@ -1,19 +1,26 @@
+# Script to generate CSV file containing a measured backlog size between certain
+# intervals defined by project length.
+
+source('include/log.r')
 source('include/project.r')
 
 conn <- connect()
+config <- get_config()
+patterns <- load_definitions('sprint_definitions.yml', config$fields)
+output_directory <- get_arg('--output', default='output')
+count <- get_arg('--count', default=4)
+
 projects <- get_projects_meta(conn,
 							  fields=c('project_id', 'name', 'quality_display_name'),
 							  metadata=list(core=T, main=T))
-intervals <- get_project_intervals(conn)
-print(projects)
+intervals <- get_project_intervals(conn, count)
 projects <- projects[projects$core & projects$main & projects$project_id %in% colnames(intervals),]
-print(projects)
 
 out <- mapply(function(project_id, name, quality_name) {
 	if (!is.na(quality_name)) {
 		name <- quality_name
 	}
-	print(name)
+	loginfo(paste("Calculating values at intervals for", name))
 	interval <- c(0, intervals[,as.character(project_id)])
 	sizes <- lapply(seq(2, length(interval)), function(index) {
 		domain <- c(interval[index-1], interval[index])
@@ -22,11 +29,14 @@ out <- mapply(function(project_id, name, quality_name) {
 		query <- paste('SELECT COUNT(DISTINCT issue.issue_id) AS backlog_size
 						FROM gros.issue
 						WHERE issue.project_id =', project_id,
-						'AND issue.updated BETWEEN', constraint)
-		dbGetQuery(conn, query)$backlog_size
+						'AND issue.updated BETWEEN', constraint,
+						'AND ${issue_not_done}')
+		item <- load_query(list(query=query), patterns)
+		dbGetQuery(conn, item$query)$backlog_size
 	})
 	return(c(name, sizes))
 }, projects$project_id, projects$name, projects$quality_display_name, SIMPLIFY=T)
 
-write.csv(t(out), file=paste('output', 'backlog_size_interval.csv', sep='/'),
-		  row.names=F, col.names=c("Project", paste("Interval", 1:4))
+write.table(t(out), sep=",",
+			file=paste(output_directory, 'backlog_size_interval.csv', sep='/'),
+			row.names=F, col.names=c("Project", paste("Interval", 1:count)))
