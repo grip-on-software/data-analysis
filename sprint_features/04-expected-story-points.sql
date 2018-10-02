6d03f3ce524103deff128fe64d1b18af432145ab
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-10-02 10:58:35 +0200

    Planned story points: Consider late removals
    Stories with no progress during the sprint but which are removed late on
    during the sprint were indeed planned to be done during the sprint and
    should thus count toward the number of planned points. This streamlines
    the consideration of older sprints and new sprints more.

--- sprint_features/04-expected-story-points.sql
+++ sprint_features/04-expected-story-points.sql
@@ -42,6 +42,7 @@ SELECT project_id, sprint_id, key, story_points FROM (
     AND (
 		later_issue.updated > ${planned_early}
 		OR later_issue.updated - issue.updated < ${planned_late} - sprint.start_date
+		OR (later_issue.updated < sprint.start_date AND newer_issue.updated > ${planned_late})
 	)
     AND later_issue.changelog_id >= issue.changelog_id
     AND (
