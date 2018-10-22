b13414e0163a42262306eaa643fd236baff91c12
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-10-22 12:46:06 +0200

    Recent sprint features: Filter tag expressions

--- features.r
+++ features.r
@@ -103,7 +103,12 @@ if (get_arg('--project', default=F)) {
 	closed <- get_arg('--closed', default=F)
 	prediction <- get_arg('--prediction', default='')
 	specifications <- yaml.load_file('sprint_features.yml')
-	all_features <- unlist(sapply(specifications$files, function(item) { item$column }))
+	all_features <- unlist(sapply(specifications$files, function(item) {
+		if (is.null(item$tag) || is.null(item$expression)) {
+			# Filter expressions that are used only as tags
+			return(item$column)
+		}
+	}))
 	if (is.na(features)) {
 		features <- all_features 
 	}
@@ -124,7 +129,7 @@ if (get_arg('--project', default=F)) {
 	}
 	meta_features <- sprint_meta[sprint_meta %in% all_features]
 	default <- c(sprint_meta, 'num_story_points', 'done_story_points',
-				  'velocity', 'lines_of_code', 'unittest_line_coverage')
+				 'velocity', 'lines_of_code', 'unittest_line_coverage')
 	default_features <- default[default %in% c(sprint_meta, features)]
 	extra_features <- features[!(features %in% default_features)]
 	if (prediction != '') {
