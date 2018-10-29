4437fe53ddd7a3d06a59b47030b53a1acfb87fd2
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-10-29 12:47:54 +0100

    Sprint features: Not done story points
    Replace stories with not done status with unfinished story points.

---
+++ sprint_features/04-not-done-points.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT issue.project_id, issue.sprint_id, issue.key, issue.story_points
+FROM gros.issue
+JOIN gros.sprint ON issue.sprint_id = sprint.sprint_id
+JOIN (SELECT issue_id, MAX(changelog_id) AS changelog_id FROM gros.issue GROUP BY issue_id) AS max_issue
+ON issue.issue_id = max_issue.issue_id
+LEFT JOIN gros.subtask ON issue.issue_id = subtask.id_subtask
+AND issue.changelog_id = max_issue.changelog_id
+WHERE issue.updated > ${sprint_open}
+AND ${s(issue_not_done)} AND ${issue_overdue}
+AND issue.story_points IS NOT NULL
+AND subtask.id_parent IS NULL
