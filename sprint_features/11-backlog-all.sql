6174786c9e264ee52b7acaf6585ca609bccf8b84
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-11-26 15:53:59 +0100

    Features: Complete backlog size before each sprint
    This provides a summarized total as well as details of the size of the
    backlog before each sprint.

---
+++ sprint_features/11-backlog-all.sql
@@ -0,0 +1,20 @@
+SELECT interval_sprint.project_id, interval_sprint.sprint_id,
+issue.key, MAX(issue.story_points) AS story_points
+FROM gros.issue
+LEFT JOIN gros.issue AS newer_issue
+	ON issue.issue_id = newer_issue.issue_id
+	AND newer_issue.changelog_id = issue.changelog_id + 1
+LEFT JOIN gros.sprint
+	ON issue.project_id = sprint.project_id
+	AND issue.sprint_id = sprint.sprint_id,
+gros.sprint AS interval_sprint
+WHERE issue.project_id = interval_sprint.project_id
+AND (sprint.sprint_id = NULL OR ${sprint_open} >= interval_sprint.start_date)
+AND ${s(issue_not_done)}
+AND ${issue_backlog}
+AND issue.updated <= interval_sprint.start_date
+AND (
+	newer_issue.updated IS NULL OR
+	newer_issue.updated > interval_sprint.start_date
+)
+GROUP BY interval_sprint.project_id, interval_sprint.sprint_id, issue.key
