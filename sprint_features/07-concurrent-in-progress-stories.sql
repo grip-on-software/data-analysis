f760b3a8b2786cfecb4e940c60de91d3d414b86c
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-11-29 14:06:25 +0100

    Features: Correct "in progress stories" values
    - Concurrent in progress stories: Only consider Story type,
      correct done issue check, update grouping to avoid over-counting
    - Duration of stories in progress: Only consider stories that are
      closed during the sprint or somewhat later after this
    - Provide details of number of concurrent stories and number of days
      in progress (plus key/story point) for the features

--- sprint_features/07-concurrent-in-progress-stories.sql
+++ sprint_features/07-concurrent-in-progress-stories.sql
@@ -1,15 +1,14 @@
-SELECT project_id, sprint_id, AVG(num_progress) AS avg_concurrent_progress FROM
-(SELECT DISTINCT project AS project_id, sprint AS sprint_id, date, COUNT(*) AS num_progress FROM
-    (SELECT project_id AS project, issue_id, sprint_id AS sprint, MIN(updated) AS date FROM gros.issue WHERE status = 3 GROUP BY project_id, issue_id, sprint_id
+SELECT project AS project_id, sprint AS sprint_id, key,
+MAX(story_points) AS story_points, COUNT(DISTINCT startdata.issue_id) AS num FROM
+    (SELECT project_id AS project, issue_id, key, sprint_id AS sprint, MAX(story_points) AS story_points, MIN(updated) AS date FROM gros.issue WHERE type = 7 AND status = 3 GROUP BY project_id, issue_id, key, sprint_id
     UNION ALL
-    SELECT project_id AS project, issue_id, sprint_id AS sprint, MIN(updated) AS date FROM gros.issue WHERE COALESCE(resolution, 0) <> 0 GROUP BY project_id, issue_id, sprint_id) AS updatedata,
-    (SELECT project_id, sprint_id, issue_id, MIN(updated) AS start_date FROM gros.issue WHERE status = 3 GROUP BY project_id, sprint_id, issue_id) AS startdata,
-    (SELECT project_id, sprint_id, issue_id, MIN(updated) AS end_date FROM gros.issue WHERE COALESCE(resolution, 0) <> 0 GROUP BY project_id, sprint_id, issue_id) AS enddata
-    WHERE startdata.issue_id = enddata.issue_id
-    AND updatedata.project = startdata.project_id
-    AND updatedata.sprint = startdata.sprint_id
-    AND updatedata.issue_id <> startdata.issue_id
-    AND date BETWEEN start_date and end_date
-    GROUP BY project, sprint, date
-) AS progressdata
-GROUP BY project_id, sprint_id
+    SELECT project_id AS project, issue_id, key, sprint_id AS sprint, MAX(story_points), MIN(updated) AS date FROM gros.issue WHERE type = 7 AND ${s(issue_done)} GROUP BY project_id, issue_id, key, sprint_id) AS updatedata,
+    (SELECT project_id, sprint_id, issue_id, MIN(updated) AS start_date FROM gros.issue WHERE type = 7 AND status = 3 GROUP BY project_id, sprint_id, issue_id) AS startdata,
+    (SELECT project_id, sprint_id, issue_id, MIN(updated) AS end_date FROM gros.issue WHERE type = 7 AND ${s(issue_done)} GROUP BY project_id, sprint_id, issue_id) AS enddata
+WHERE startdata.issue_id = enddata.issue_id
+AND startdata.sprint_id = enddata.sprint_id
+AND updatedata.project = startdata.project_id
+AND updatedata.sprint = startdata.sprint_id
+AND updatedata.issue_id <> startdata.issue_id
+AND date BETWEEN start_date and end_date
+GROUP BY project, sprint, key
--- sprint_features/07-duration-in-progress-stories.sql
+++ sprint_features/07-duration-in-progress-stories.sql
@@ -1,7 +1,9 @@
-SELECT startdata.project_id, startdata.sprint_id, AVG(EXTRACT(day FROM end_date - start_date)) AS avg_duration_progress FROM
-(SELECT project_id, issue_id, sprint_id, MIN(updated) AS start_date FROM gros.issue WHERE ${issue_story} AND status = 3 GROUP BY project_id, issue_id, sprint_id) AS startdata,
-(SELECT project_id, issue_id, sprint_id, MIN(updated) AS end_date FROM gros.issue WHERE ${s(issue_done)} GROUP BY project_id, issue_id, sprint_id) AS enddata
+SELECT startdata.project_id, startdata.sprint_id, startdata.key, enddata.story_points, EXTRACT(day FROM enddata.end_date - startdata.start_date) AS num FROM
+(SELECT project_id, issue_id, key, sprint_id, MIN(updated) AS start_date FROM gros.issue WHERE ${issue_story} AND status = 3 GROUP BY project_id, issue_id, key, sprint_id) AS startdata,
+(SELECT project_id, issue_id, key, sprint_id, MAX(story_points) AS story_points, MIN(updated) AS end_date FROM gros.issue WHERE ${s(issue_done)} GROUP BY project_id, issue_id, key, sprint_id) AS enddata, gros.sprint
 WHERE startdata.issue_id = enddata.issue_id
 AND startdata.sprint_id = enddata.sprint_id
+AND startdata.project_id = sprint.project_id
+AND startdata.sprint_id = sprint.sprint_id
 AND enddata.end_date > startdata.start_date
-GROUP BY startdata.project_id, startdata.sprint_id
+AND enddata.end_date <= ${sprint_close} + (sprint.end_date - sprint.start_date)/7
