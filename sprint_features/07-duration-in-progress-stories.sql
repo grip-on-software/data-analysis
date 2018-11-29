07c84a9dcc35d6831b21d7661b96af2c19db736d
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2018-11-29 10:36:46 +0100

    Features: Correct duration in progress stories
    - Avoid end dates that are earlier than start date
    - Only consider stories finished within the sprint
    - Adjust means of detecting stories and done issues

--- sprint_features/07-duration-in-progress-stories.sql
+++ sprint_features/07-duration-in-progress-stories.sql
@@ -1,5 +1,7 @@
 SELECT startdata.project_id, startdata.sprint_id, AVG(EXTRACT(day FROM end_date - start_date)) AS avg_duration_progress FROM
-(SELECT project_id, issue_id, sprint_id, MIN(updated) AS start_date FROM gros.issue WHERE type = 7 AND status = 3 GROUP BY project_id, issue_id, sprint_id) AS startdata,
-(SELECT project_id, issue_id, sprint_id, MIN(updated) AS end_date FROM gros.issue WHERE COALESCE(resolution, 0) <> 0 GROUP BY project_id, issue_id, sprint_id) AS enddata
+(SELECT project_id, issue_id, sprint_id, MIN(updated) AS start_date FROM gros.issue WHERE ${issue_story} AND status = 3 GROUP BY project_id, issue_id, sprint_id) AS startdata,
+(SELECT project_id, issue_id, sprint_id, MIN(updated) AS end_date FROM gros.issue WHERE ${s(issue_done)} GROUP BY project_id, issue_id, sprint_id) AS enddata
 WHERE startdata.issue_id = enddata.issue_id
+AND startdata.sprint_id = enddata.sprint_id
+AND enddata.end_date > startdata.start_date
 GROUP BY startdata.project_id, startdata.sprint_id
