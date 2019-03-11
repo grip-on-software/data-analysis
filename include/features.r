5996c782ac8e1aa36862e2997335a193dedac546
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2019-03-11 13:33:56 +0100

    Features: Avoid error if too few sprints for 7-sprint velocity in feature metadata

--- include/features.r
+++ include/features.r
@@ -825,7 +825,11 @@ get_expression_attrs <- function(expression, vars) {
                 aux <- substr(ref, regexpr("object ", ref) + 8,
                               regexpr(" not found", ref) - 2)
                 assign(as.character(aux), vars[[aux]], envir=environment)
-            } else {
+            }
+            else if (length(grep("wrong embedding dimension", ref[1])) > 0) {
+                return()
+            }
+            else {
                 stop(paste("expression", expr, "could not be evaluated:",
                            ref[1], "but a missing variable was not identified"))
             }
