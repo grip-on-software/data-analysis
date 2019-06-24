42ccda3a5391b3e28b670aa54e8e4a3b8093215d
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2019-06-24 12:02:20 +0200

    Features: Optimize metric count features

--- sprint_features/09-metric-count.sql
+++ sprint_features/09-metric-count.sql
@@ -2,8 +2,7 @@ SELECT ${f(join_cols, "sprint_metrics")}, COUNT(*) AS num_metrics
 FROM (
     SELECT DISTINCT ${f(join_cols, "metric_value")}, metric_value.metric_id
     FROM gros.metric_value
-    JOIN gros.${t("sprint")} ON ${j(join_cols, "metric_value", "sprint")}
-    WHERE metric_value."value" <> -1
+    WHERE metric_value."value" <> -1 AND metric_value.sprint_id <> 0
 	${s(project_condition, project="metric_value")}
 ) AS sprint_metrics
 ${g(join_cols, "sprint_metrics")}
