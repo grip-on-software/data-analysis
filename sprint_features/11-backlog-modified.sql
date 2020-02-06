e9b0e6674ccce49ee7302fd4d09266fa531cd980
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2020-02-06 12:06:48 +0100

    Feature: Modified story points on backlog

---
+++ sprint_features/11-backlog-modified.sql
@@ -0,0 +1,21 @@
+SELECT ${f(join_cols, "sprint", alias=T, sprint="interval_sprint")}, ${s(issue_key)} AS key,
+	MAX(${s(story_points)} - COALESCE(${s(story_points, issue="older_issue")}, 0)) AS story_points
+FROM gros.${t("issue")}
+JOIN gros.${t("issue")} AS older_issue
+ON ${j(issue_next_changelog, "issue", "older_issue")}
+LEFT JOIN gros.${t("sprint")}
+ON ${j(join_cols, "issue", "sprint")}
+JOIN gros.${t("sprint")} AS interval_sprint
+ON ${j(join_cols, "issue", "interval_sprint", 1)}
+${s(issue_join)}
+WHERE (
+    ${f(join_cols, "sprint", mask=2, alias="alias")} IS NULL
+    OR ${s(issue_open)}
+    OR ${s(sprint_close)} >= ${s(sprint_close, sprint="interval_sprint")}
+)
+AND ${s(issue_backlog)}
+AND ${t("issue")}.updated <= ${s(sprint_close, sprint="interval_sprint")}
+AND ${t("issue")}.updated >= ${s(sprint_open, sprint="interval_sprint")}
+AND ${t("issue")}.story_points <> older_issue.story_points
+${s(project_condition, project="issue")}
+${g(join_cols, "sprint", f("issue_key"), sprint="interval_sprint")}
