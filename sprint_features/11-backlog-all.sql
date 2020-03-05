7de09dbf4ae4193e536f7de1cc69243118d7b252
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2020-03-05 18:11:38 +0100

    Backlog: Ignore points from closed stories outside of sprint

--- sprint_features/11-backlog-all.sql
+++ sprint_features/11-backlog-all.sql
@@ -10,8 +10,7 @@ JOIN gros.${t("sprint")} AS interval_sprint
 ON ${j(join_cols, "issue", "interval_sprint", 1)}
 ${s(issue_join)}
 WHERE (
-    ${f(join_cols, "sprint", mask=2, alias="alias")} IS NULL
-    OR ${s(issue_open)}
+    ${s(issue_open)}
     OR ${s(sprint_close)} >= ${s(sprint_close, sprint="interval_sprint")}
 )
 AND ${s(issue_backlog)}
