4e08d44bea203137d883aef6ea833104836f1ddc
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2021-02-16 15:16:01 +0100

    Backlog modified story points: Improve performance
    Move conditions to join clauses to decrease the complexity of the joins
    and thus the size of the combined tables.

--- sprint_features/11-backlog-modified.sql
+++ sprint_features/11-backlog-modified.sql
@@ -4,10 +4,13 @@ SELECT ${f(join_cols, "sprint", alias=T, sprint="interval_sprint")}, ${s(issue_k
 FROM gros.${t("issue")}
 JOIN gros.${t("issue")} AS older_issue
 ON ${j(issue_next_changelog, "issue", "older_issue")}
+AND COALESCE(${t("issue")}.story_points, 0) <> COALESCE(older_issue.story_points, 0)
 LEFT JOIN gros.${t("sprint")}
 ON ${j(join_cols, "issue", "sprint")}
 JOIN gros.${t("sprint")} AS interval_sprint
 ON ${j(join_cols, "issue", "interval_sprint", 1)}
+AND ${t("issue")}.updated <= ${s(sprint_close, sprint="interval_sprint")}
+AND ${t("issue")}.updated >= ${s(sprint_open, sprint="interval_sprint")}
 ${s(issue_join)}
 WHERE (
     ${f(join_cols, "sprint", mask=2, alias="alias")} IS NULL
@@ -15,9 +18,6 @@ WHERE (
     OR ${s(sprint_close)} >= ${s(sprint_close, sprint="interval_sprint")}
 )
 AND ${s(issue_backlog)}
-AND ${t("issue")}.updated <= ${s(sprint_close, sprint="interval_sprint")}
-AND ${t("issue")}.updated >= ${s(sprint_open, sprint="interval_sprint")}
-AND COALESCE(${t("issue")}.story_points, 0) <> COALESCE(older_issue.story_points, 0)
 ${s(project_condition, project="issue")}
 ${s(filter_condition)}
 ${g(join_cols, "sprint", f("issue_key"), sprint="interval_sprint")}
